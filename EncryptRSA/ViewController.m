a000db6b79bc712c28ede3688914e5e4e737e112
huangxiong <huangxionging@163.com>
2016-07-06 09:33:23 +0800

    更新
    更新

--- EncryptRSA/ViewController.m
+++ EncryptRSA/ViewController.m
@@ -22,6 +22,15 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    SecKeyDecrypt(<#SecKeyRef  _Nonnull key#>, <#SecPadding padding#>, <#const uint8_t * _Nonnull cipherText#>, <#size_t cipherTextLen#>, <#uint8_t * _Nonnull plainText#>, <#size_t * _Nonnull plainTextLen#>)
+    SecKeyEncrypt(<#SecKeyRef  _Nonnull key#>, <#SecPadding padding#>, <#const uint8_t * _Nonnull plainText#>, <#size_t plainTextLen#>, <#uint8_t * _Nonnull cipherText#>, <#size_t * _Nonnull cipherTextLen#>)
+}
+
++ (SecKeyRef) getPublicKey: (NSData *) data {
+    SecCertificateRef myCertificate = SecCertificateCreateWithData(kCFAllocatorDefault, (__bridge CFDataRef) data);
+    
+    SecKeyRef publicKey = secp
+    
 }
 
 @end
